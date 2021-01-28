939dce3967a7133041132817dddeea740d6ca133
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2021-01-28 14:51:06 +0800

    up

--- utils/util_darwin.go
+++ utils/util_darwin.go
@@ -9,3 +9,9 @@ func StringToWCharPtr(str string) uintptr {
 	utf32 := []rune(str)
 	return uintptr(unsafe.Pointer(&utf32[0]))
 }
+
+//WCharSplitToString mac
+func WCharSplitToString(buff []byte) string {
+	u32 := (*[1 << 29]rune)(unsafe.Pointer(&buff[0]))[0 : len(buff)/4 : len(buff)/4]
+	return string(u32)
+}
--- utils/util_windows.go
+++ utils/util_windows.go
@@ -10,3 +10,9 @@ func StringToWCharPtr(str string) uintptr {
 	strp, _ := syscall.UTF16PtrFromString(str)
 	return uintptr(unsafe.Pointer(strp))
 }
+
+//WCharSplitToString ...
+func WCharSplitToString(buff []byte) string {
+	u16 := (*[1 << 29]uint16)(unsafe.Pointer(&buff[0]))[0 : len(buff)/2 : len(buff)/2]
+	return syscall.UTF16ToString(u16)
+}
