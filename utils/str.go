5d61b35b92e23c87864cd789c8e721d80bf96f7a
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2021-03-16 13:04:20 +0800

    up

--- utils/str.go
+++ utils/str.go
@@ -7,3 +7,22 @@ func StringToFloat(s string) (ret float64) {
 	fmt.Sscanf(s, "%f", &ret)
 	return
 }
+
+func StringToUnicode(s string) string {
+	us := ""
+	for _, a := range s {
+		us += fmt.Sprintf(`\u%04x`, a)
+	}
+	return us
+}
+
+func UnicodeToString(us string) string {
+	s := ""
+	l := len(us) / 6
+	for i := 0; i < l; i++ {
+		var c rune
+		fmt.Sscanf(us[i*6:i*6+6], `\u%04x`, &c)
+		s += string(c)
+	}
+	return s
+}
